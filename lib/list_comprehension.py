7e27cb1db6fef4bca1533b8f5949ced7e1752165
Christian Garcia <bones192nd@gmail.com>
2023-05-25 09:25:10 -0400

    lab submission

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list = [element for element in num_list if element % 2 == 0]
+    return num_list
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list = [word + "!" for word in sentence_list]
+    return sentence_list
